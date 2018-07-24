262562cbfbb924e03353e7f4209c05b11bec3e7b
Morgan Bauer <mbauer@us.ibm.com>
2018-07-24 11:35:24 -0700

    print the logs somewhere
     - logs go to stdout now, instead of nowhere
    
    Example log:
    {"timestamp":"1532457100.377474785","source":"blockhead-broker","message":"blockhead-broker.reading config file \"/creds.json\"","log_level":1,"data":{}}

--- cmd/broker/main.go
+++ cmd/broker/main.go
@@ -1,24 +1,28 @@
 package main
 
 import (
+	"fmt"
 	"net/http"
 	"os"
 
 	"code.cloudfoundry.org/lager"
+	"github.com/pivotal-cf/brokerapi"
+
 	"github.com/jberkhahn/blockhead/pkg/broker"
 	"github.com/jberkhahn/blockhead/pkg/config"
-	"github.com/pivotal-cf/brokerapi"
 )
 
 func main() {
+	logger := lager.NewLogger("blockhead-broker")
 	configFilepath := os.Args[1]
+	logger.RegisterSink(lager.NewWriterSink(os.Stdout, lager.DEBUG))
+	logger.Debug(fmt.Sprintf("reading config file %q", configFilepath))
 	cfg, err := config.NewConfig(configFilepath)
 	if err != nil {
 		panic(err)
 	}
 
 	broker := broker.BlockheadBroker{}
-	logger := lager.NewLogger("blockhead-broker")
 	creds := brokerapi.BrokerCredentials{
 		Username: cfg.Username,
 		Password: cfg.Password,
